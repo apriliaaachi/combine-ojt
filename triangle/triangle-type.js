3979b6a0e73a3818eb860f54d26feb8082b6ace6
apriliaaachi <achiaprilia.aa@gmail.com>
2021-08-09 02:11:42 +0700

    fungsi specifyTriangleType

--- triangle/triangle-type.js
+++ triangle/triangle-type.js
@@ -1,5 +1,26 @@
-/* 
-  to aryo
-  move your code here and create function
-    create all of your function (triangle-type)
-*/
+function specifyTriangleType(sideArray) {
+    sideArray.sort(function(a, b) {
+        return b - a;
+    });
+    var sideA = sideArray[0]
+    var sideB = sideArray[1]
+    var sideC = sideArray[2]
+
+    if (sideA ** 2 == sideB ** 2 + sideC ** 2) {
+        if (sideB == sideC) {
+            return "Segitiga Siku-Siku Sama Kaki"
+        } else {
+            return "Segitiga Siku-Siku"
+        }
+    } else if (sideA == sideB && sideB == sideC) {
+        return "Segitiga Sama Sisi"
+    } else if (sideA == sideB || sideB == sideC || sideC == sideA) {
+        return "Segitiga Sama Kaki"
+    } else {
+        return "Segitiga Sembarang"
+    }
+}
+
+module.exports = {
+    specifyTriangleType
+}
